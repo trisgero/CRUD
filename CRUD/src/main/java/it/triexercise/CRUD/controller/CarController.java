d94d23c4d4f5bb05481e4473e8efb87183d8ebbd
DESKTOP-LPAFG9R\Tri <tristanpgeronimo@gmail.com>
2022-06-16 21:35:38 +0200

    Added done exercise, tested

--- CRUD/src/main/java/it/triexercise/CRUD/controller/CarController.java
+++ CRUD/src/main/java/it/triexercise/CRUD/controller/CarController.java
@@ -5,6 +5,7 @@ import it.triexercise.CRUD.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +15,7 @@ public class CarController {
 
     @Autowired
     private CarRepository carRepository;
+
 
     @PostMapping
     public void createCar(@RequestBody Car car) {
@@ -25,17 +27,36 @@ public class CarController {
         return carRepository.findAll();
     }
 
-//    @GetMapping("/{id}")
-//    public Optional<Car> getCarById(@PathVariable int id) {
-//        if (!carRepository.existsById(id)) {
-//            return null;
-//        }
-//    }
+    @GetMapping("/{id}")
+    public Optional<Car> getCarById(@PathVariable int id) {
 
-//    @PutMapping("/{id}")
-//    public void editCarById(@PathVariable int id, @RequestParam Car car) {
-//
-//    }
-//
-//    @DeleteMapping
+        if (!carRepository.existsById(id)) {
+            Car car = new Car();
+            return Optional.of(car);
+        }
+        return carRepository.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Car editCarTypeById(@PathVariable int id, @RequestParam String type) {
+        Car car = new Car();
+        if (!carRepository.existsById(id))
+            return car;
+        car = carRepository.getReferenceById(id); // used getReferenceById() as getById() is deprecated
+        car.setType(type);
+        return carRepository.saveAndFlush(car);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCarById(@PathVariable int id, HttpServletResponse response) {
+        if (!carRepository.existsById(id))
+            response.setStatus(409);
+        carRepository.deleteById(id);
+    }
+
+    @DeleteMapping
+    public void deleteAllCars() {
+        carRepository.deleteAll();
+    }
+
 }
